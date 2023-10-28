ade64a65fd6ce190ae5337e54821bd5eb06ae4b2
Abanoubmedhatseif <146996493+Abanoubmedhatseif@users.noreply.github.com>
2023-10-28 21:04:08 +0200

    question 20.c

--- Assignment 1/question 20.c
+++ Assignment 1/question 20.c
@@ -3,19 +3,19 @@
 
 int main()
 {
-    int i, space, rows, k = 0;
+    int i, space, rows, a = 0;
    printf("Enter rows \n");
    scanf("%d", &rows);
-   for (i=1; i<= rows; ++i, k = 0)
+   for (i=1; i<= rows; ++i, a = 0)
     {
       for (space = 1; space <= rows - i; ++space)
           {
              printf("  ");
           }
-      while (k < 2 * i - 1)
+      while (a < 2 * i - 1)
           {
              printf("* ");
-             k++;
+             a++;
           }
         printf("\n");
     }
